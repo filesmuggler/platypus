3c922d5ff570df8edec74db4c38d3671108d20a0
filesmuggler <krzste09@outlook.com>
2019-03-18 12:16:59 +0100

    ir control basic diode works

--- test-platypus-krzysztof/test-platypus-krzysztof.ino
+++ test-platypus-krzysztof/test-platypus-krzysztof.ino
@@ -1,101 +1,57 @@
-/*************************************** 
-Waveshare AlphaBot Car Infrared Line Tracking
-
-CN: www.waveshare.net/wiki/AlphaBot
-EN: www.waveshare.com/wiki/AlphaBot
-****************************************/
 #include "TRSensors.h"
+#include "IRremote.h"
+
+#define RCV_PIN 4
+IRrecv irrecv(RCV_PIN);
+decode_results results;
+
+#define ML 5
+#define MR 6
+
+#define ML_IN1 A1
+#define ML_IN2 A0
+#define MR_IN3 A2
+#define MR_IN4 A3
 
 #define NUM_SENSORS 5
-// sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
-TRSensors trs =   TRSensors();
-unsigned int sensorValues[NUM_SENSORS];
-unsigned int last_proportional = 0;
-long integral = 0;
+TRSensors sensors = TRSensors();
+unsigned int sensorsValues[NUM_SENSORS];
 
-int ENA = 5;
-int ENB = 6;
-int IN1 = A1;
-int IN2 = A0;
-int IN3 = A2;
-int IN4 = A3;
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("TRSensor example");
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA,0);
-  analogWrite(ENB,0);
-  
-  for (int i = 0; i < 400; i++)                  // make the calibration take about 10 seconds
-  {
-    trs.calibrate();                             // reads all sensors 10 times
-  }
-  Serial.println("calibrate done");
-  
-  // print the calibration minimum values measured when emitters were on
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(trs.calibratedMin[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  
-  // print the calibration maximum values measured when emitters were on
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(trs.calibratedMax[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  delay(1000);
+bool if_start = false;
+bool if_stop = true;
+
+void setup() {
+  irrecv.enableIRIn();
+  pinMode(ML, OUTPUT);
+  pinMode(MR, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
-
-void loop()
-{
-  // Get the position of the line.  Note that we *must* provide
-  // the "sensors" argument to read_line() here, even though we
-  // are not interested in the individual sensor readings.
-  unsigned int position = trs.readLine(sensorValues);
-  //  for (unsigned char i = 0; i < NUM_SENSORS; i++)
-  //  {
-  //    Serial.print(sensorValues[i]);
-  //    Serial.print('\t');
-  //  }
-  //Serial.println(position); // comment this line out if you are using raw values
-
-  // The "proportional" term should be 0 when we are on the line.
-  int proportional = (int)position - 2000;
-  // improve performance.
-  int power_difference = proportional/15; //+derivative;  
-
-  // Compute the actual motor settings.  We never set either motor
-  // to a negative value.
-  const int maximum =100;
-
-  if (power_difference > maximum)
-    power_difference = maximum;
-  if (power_difference < - maximum)
-    power_difference = - maximum;
-//    Serial.println(power_difference);
-  if (power_difference < 0)
-  {
-    analogWrite(ENB,maximum + power_difference);
-    analogWrite(ENA,maximum);
+void loop() {
+  if (!if_start) {
+    // wait for IR control to start the race
+    if (irrecv.decode(&results)) {
+      if (results.value == 16724175) {
+        if_start = true;
+        if_stop = false;
+        digitalWrite(13, HIGH);
+      }
+      irrecv.resume();
+    }
   }
-  else
-  {
-    analogWrite(ENB,maximum);
-    analogWrite(ENA,maximum - power_difference);
-  }    
+  else if (!if_stop) {
+    // check for IR control to stop the car
+    if (irrecv.decode(&results)) {
+      if (results.value == 16718055) {
+        if_start = false;
+        if_stop = true;
+        digitalWrite(13, LOW);
+      }
+      irrecv.resume();
+    }
+    
+    // drive the car
+    
+  }
 }
