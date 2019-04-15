39cfc86d40df89caa3e8b880d6cc6b6d46e123af
filesmuggler <krzste09@outlook.com>
2019-04-15 10:17:45 +0200

    rewriting code clean

--- Platypus/Platypus.ino
+++ Platypus/Platypus.ino
@@ -1,41 +1,69 @@
-/*
-  MIT License
-  ---
-  Arduino Waveshare Alphabot Robot Project
-  Program purpose: Main program
-  ---
-  @author: Krzysztof Stezala
-  ---
-  Provided by CybAiR Science Club at 
-  Institute of Control, Robotics and Information Engineering of
-  Poznan University of Technology  
-*/
+#include "TRSensors.h"
+#include "IRremote.h"
 
-#include "PID_Library.h"
+/* remote control */
+#define RCV_PIN 4
+IRrecv irrecv(RCV_PIN);
+decode_results results;
 
-PID_Controller pid_c;
+/* motors */
+#define ML 5
+#define MR 6
+#define ML_IN1 A1
+#define ML_IN2 A0
+#define MR_IN3 A2
+#define MR_IN4 A3
 
-typdef struct{
-  int input;
-  short weight;
-} SensorNode;
+/* sensors */
+#define NUM_SENSORS 5
+TRSensors sensors = TRSensors();
+unsigned int sensorsValues[NUM_SENSORS];
 
-SensorNode sensors[5]={};
+/* global variables for robot start-stop control */
+bool if_start = false;
+bool if_stop = true;
 
 void setup(){
+    Serial.begin(9600);
 
-    pid_c.setTunings(1.0,1.0,1.0);
+    /* IR RC enabled */
+    irrecv.enableIRIn();
+
+    /* setting up motors */
+    pinMode(ML, OUTPUT);
+    pinMode(MR, OUTPUT);
+    pinMode(ML_IN1, OUTPUT);
+    pinMode(ML_IN2, OUTPUT);
+    pinMode(MR_IN3, OUTPUT);
+    pinMode(MR_IN4, OUTPUT);
+    digitalWrite(ML_IN1, HIGH);
+    digitalWrite(ML_IN2, LOW);
+    digitalWrite(MR_IN3, HIGH);
+    digitalWrite(MR_IN4, LOW);
+    analogWrite(ML, 0);
+    analogWrite(MR, 0);
+
+    /* sensors calibration */
+    for (int i = 0; i < 200; i++) {
+        sensors.calibrate();
+        Serial.print("Calibrating...");
+        Serial.print((float)i / 200.0 * 100);
+        Serial.println("% complete.");
+    }
+    Serial.println("Calibration finished.");
     
-    //create PID controller
-    //create array of sensors
-    //initialize pins
-    //in case of auto control: delay after power up
-    //in case of remote control: wait for comm
+
+    delay(1000);
 }
 
 void loop(){
-    //read analog values
-    //sort values
-    //compute PID algorithm
-    //correct path and speed
+
+}
+
+void Compute(){
+
+}
+
+void SetTunings(){
+
 }
